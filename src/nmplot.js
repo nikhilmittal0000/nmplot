a6a10ec90358e9e30eedb2e43785b152586bb12c
Nikhil Mittal <nikhilmittal0000@gmail.com>
2022-05-02 11:29:12 +0530

    Added Origin concept in shape

--- src/nmplot.js
+++ src/nmplot.js
@@ -16,6 +16,9 @@
     }
     var NmplotShape = function (shape, dim) {
         this.points = [];
+        this.scaleX = 10;
+        this.scaleY = 10;
+        this.defaultColor = "black";
 
         switch (shape) {
             case "rectangle":
@@ -26,6 +29,7 @@
                     coord(dim.l1, dim.l2),
                     coord(0, dim.l2),
                 ];
+                this.origin = coord(dim.l1 / 2, dim.l2 / 2);
                 break;
             default:
                 this.shape = shape;
@@ -36,29 +40,14 @@
         this.points.push(coord(x, y));
     };
     var NmplotCanvas = function (canvasId, container) {
-        const scale = 10;
+        this.name = "NmplotCanvas";
+        this.description = "Object to render Nmplot container on canvas";
         this.canvas = document.getElementById(canvasId);
         this.ctx = this.canvas.getContext("2d");
-        this.canvas.width = 1000;
-        this.canvas.height = 500;
-        this.setBackground();
-        var shapes = container.shapes;
-        this.ctx.fillStyle = "black";
-        for (var i = 0; i < shapes.length; i++) {
-            console.log(shapes[i]);
-            var points = shapes[i].points;
-            this.ctx.beginPath();
-            for (var j = 0; j < points.length; j++) {
-                if (j === 0) {
-                    this.ctx.moveTo(points[j].x * scale, points[j].y * scale);
-                } else {
-                    this.ctx.lineTo(scale * points[j].x, scale * points[j].y);
-                }
-            }
-            this.ctx.fill();
-        }
-        this.name = "NmplotCanvas";
-        this.description = "Utility to draw and animate shape on canvas";
+        this.nmplotContainer = container;
+        this.setCanvasDims(1000, 500);
+        this.resetOrigin();
+        this.render();
     };
     global.NmplotCanvas = NmplotCanvas;
     NmplotCanvas.prototype.setBackground = function () {
@@ -66,5 +55,43 @@
         // ctx = canvas.getContext("2d");
         this.ctx.fillStyle = "#DCDCDC";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+    NmplotCanvas.prototype.render = function () {
+        this.setBackground();
+        var shapes = this.nmplotContainer.shapes;
+
+        for (var i = 0; i < shapes.length; i++) {
+            var shape = shapes[i];
+            this.ctx.fillStyle = shape.defaultColor;
+            console.log(shape);
+            var points = shape.points;
+            this.ctx.beginPath();
+            for (var j = 0; j < points.length; j++) {
+                if (j === 0) {
+                    this.ctx.moveTo(
+                        this.originX +
+                            shape.scaleX * (points[j].x - shape.origin.x),
+                        this.originY +
+                            shape.scaleY * (points[j].y - shape.origin.y)
+                    );
+                } else {
+                    this.ctx.lineTo(
+                        this.originX +
+                            shape.scaleX * (points[j].x - shape.origin.x),
+                        this.originY +
+                            shape.scaleY * (points[j].y - shape.origin.y)
+                    );
+                }
+            }
+            this.ctx.fill();
+        }
+    };
+    NmplotCanvas.prototype.resetOrigin = function () {
+        this.originX = Math.floor(this.canvas.width / 2);
+        this.originY = Math.floor(this.canvas.height / 2);
+    };
+    NmplotCanvas.prototype.setCanvasDims = function (x, y) {
+        this.canvas.width = x;
+        this.canvas.height = y;
     };
 })(window);
